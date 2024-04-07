21a2768171c06eb652a2cd0646345367c0a93ba9
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:08:42 +0000

    test_lca_code_editing__code-editing-metrics-x6c85ba4e9303a878__id_143

--- tests/middleware/test_middleware.py
+++ tests/middleware/test_middleware.py
@@ -2,9 +2,16 @@ from starlette.middleware import Middleware
 
 
 class CustomMiddleware:
-    pass
+    def __init__(self, app: Any, **options: typing.Any) -> None:
+        self.app = app
+        self.options = options
 
+    def __iter__(self) -> typing.Iterator[typing.Any]:
+        as_tuple = (self.app, self.options)
+        return iter(as_tuple)
 
-def test_middleware_repr():
-    middleware = Middleware(CustomMiddleware)
-    assert repr(middleware) == "Middleware(CustomMiddleware)"
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        option_strings = [f"{key}={value!r}" for key, value in self.options.items()]
+        args_repr = ", ".join([self.app.__class__.__name__] + option_strings)
+        return f"{class_name}({args_repr})"
