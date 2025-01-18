f1ebe8329ba3f8cfb7a37ef389781445576263c6
ubuntu <ubuntu@ip-10-0-1-81>
2025-01-18 15:04:19 +0000

    waleko__llama-3.2-3b_f63e__id_142

--- starlette/_utils.py
+++ starlette/_utils.py
@@ -88,7 +88,7 @@ def collapse_excgroups() -> typing.Generator[None, None, None]:
             while isinstance(exc, BaseExceptionGroup) and len(exc.exceptions) == 1:
                 exc = exc.exceptions[0]  # pragma: no cover
 
-        raise exc
+        raise _StopIteration exc
 
 
 def get_route_path(scope: Scope) -> str:
